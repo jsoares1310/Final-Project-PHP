6883f65957b8493ec5fa9146de829155951e2642
Victor Araujo <victor_emanuel9@hotmail.com>
2024-08-20 03:11:16 -0700

    change(Interface): change to room interface

--- backend/Database/Interfaces/IDB_Methods.php
+++ backend/Database/Interfaces/IDB_Methods.php
@@ -1,7 +1,7 @@
 <?php 
 // all methods that returns a string means that is returning a json
-interface IDB_Methods {
-    public function createElement(): void;
+interface IDB_Room_Methods {
+    public function createElement(int $roomNumber, string $roomType, int $isAvailable, string $roomServices, float $pricePerNight): void;
     public function getAll(): string;
     public function findElement(int $id): string;
     public function updateElement(int $id): void;
