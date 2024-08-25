20f068700d2d5a7d3b832257ff21fb576f449e90
Victor Araujo <victor_emanuel9@hotmail.com>
2024-08-25 01:04:21 -0700

    change(AdminClass): implement add_user

--- backend/Classes/AdminClass.php
+++ backend/Classes/AdminClass.php
@@ -1,13 +1,30 @@
 <?php 
+require_once("./Database/Staff.db.php");
+require_once("./Classes/AbstractClasses/UserClass.php");
 class Admin extends User {
+    private Staff $staff;
+    private string $log_file;
+
+    private string $role;
 
     public function __construct(int $uid, string $fname, string $lname, string $email, string $phone) {
         parent::__construct($uid, $fname, $lname, $email, $phone);
+        $this->role = "admin";
+        $this->log_file = "admin-controller.txt";
+        $this->staff = new Staff();
     }
 
     // Method to manage users (add/remove/lock/unlock)
-    public function add_user(int $uid, string $fname, string $lname, string $email, string $phone) {
-
+    public function add_user(string $pwd) {
+        try {
+            $this->staff->createElement($this->fname, $this->lname, $this->phone, $this->email, $pwd, $this->role);
+            if (http_response_code() == 201) {
+                echo "Staff created";
+            }
+            return;
+        } catch(Exception $error) {
+            $this->staff->logMessage($this->log_file, $error->getMessage());
+        }
     }
 
     public function edit_user(int $uid) {
@@ -38,6 +55,10 @@ class Admin extends User {
 
     public function update_room(int $roomId, array $roomDetails) {
     
+    }
+
+    public function closeConnection() {
+        $this->staff->close();
     }
 }
 ?>
