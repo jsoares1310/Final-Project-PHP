2d5a43db064ec2aa40c8f3facb0742531a2d3941
Victor Araujo <victor_emanuel9@hotmail.com>
2024-08-25 01:03:17 -0700

    change(index.php): setup routes and migration

--- backend/index.php
+++ backend/index.php
@@ -1,49 +1,31 @@
-<?php 
-require("./Database/Room.db.php");
+<?php
+require_once("./Database/Migrations/Migrations.php");
+new Migrations();
+$uri = $_SERVER['REQUEST_URI'];
+print_r($uri);
+// print_r($_SERVER);
 
-$db = new Room();
-// echo "<br/>";
-// print_r($db->getAll());
-// $db->createElement(112, 'single', 1, 'WiFi, TV', 7500.90);
-print_r($db->findElement(112));
-// $db->deleteElement(105);
-// $db->updateElement(101, [
-//         // "room_number" => 103,
-//         "room_type" => "double",
-//         // "is_available" => 0,
-//         // "room_service" => "WiFi, TV, Air, Bar",
-//         "price_per_night" => 120.55
-//     ]);
-$db->close();
-
-// require ("./Database/Customer.db.php");
-
-// $cusDb = new Customer();
-
-
-// $cusDb->createElement('victor', 'araujo', '1234567890', 'victor@outlook.com', 'admin');
-// print_r($cusDb->getAll());
-// print_r($cusDb->findElement("john.doe@example.com"));
-// $cusDb->deleteElement("john.doe@example.com");
-// $cusDb->updateElement("john.doe@example.com", [
-//     "last_name" => 'eoD',
-// ]);
-
-
-// $cusDb->close();
-
-// require("./Database/Staff.db.php");
-
-// $stdb = new Staff();
-
-// $stdb->createElement('OhMah', 'Gah', '1234567890', 'test@test.com', '123456', 'staff');
-// print_r($stdb->getAll());
-// print_r($stdb->findElement('test@test.com'));
-// $stdb->updateElement('test@test.com', [
-//     "email" => 'newTest@test.com'
-// ]);
-// $stdb->deleteElement('newTest@test.com');
-
-// $stdb->close();
+switch ($uri) {
+    case '/login':
+        echo ' Not implemented yet';
+        break;
+    
+    case '/customer':
+        echo ' Not implemented yet';
+        break;
+    case '/rooms':
+        echo ' Not implemented yet';
+        break;
+    case '/booking':
+        echo ' Not implemented yet';
+        break;
+    case '/staff':
+        echo ' Not implemented yet';
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(['error' => 'Route not found']);
+        break;
+}
 
 ?>
