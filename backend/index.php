d7ed11d2118bbda9fc9a295bfbf205c9a4ea4f22
Victor Araujo <victor_emanuel9@hotmail.com>
2024-08-26 16:53:31 -0700

    change(index.php) add some new routes

--- backend/index.php
+++ backend/index.php
@@ -10,19 +10,31 @@ switch ($uri) {
     case '/login':
         echo ' Not implemented yet';
         break;
+
+    case '/staff/login':
+        echo ' Not implemented yet';
+        break;
+
+    case '/register':
+        echo ' Not implemented yet';
+        break;
     
     case '/customer':
         echo ' Not implemented yet';
         break;
+
     case '/rooms':
         echo ' Not implemented yet';
         break;
+
     case '/booking':
         echo ' Not implemented yet';
         break;
+
     case '/staff':
         echo ' Not implemented yet';
         break;
+
     default:
         http_response_code(404);
         echo json_encode(['error' => 'Route not found']);
