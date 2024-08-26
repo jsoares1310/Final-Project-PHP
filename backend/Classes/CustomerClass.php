81c67ad4c04132a6aff02b4a62c1a1ef7c53838c
dieceva <dieceva96@gmail.com>
2024-08-25 20:25:24 -0700

    feat(CustomerClass): added function to add funds to customer wallet

--- backend/Classes/CustomerClass.php
+++ backend/Classes/CustomerClass.php
@@ -1,24 +1,60 @@
 <?php
-class Customer extends User{
-    private float $wallet;
+require_once("./Classes/Customer.db.php");
+require_once("./Classes/AbstractClasses/UserClass.php");
+
+class Customer extends User implements IDB_Customer_Methods{
+    private float $wallet_balance;
+    private string $log_file;
     
-    public function __construct(int $uid, string $fname, string $lname, string $email, string $phone, float $wallet) {    
-        $this->wallet=$wallet;
+    public function __construct(int $uid, string $fname, string $lname, string $email, string $phone, float $wallet_balance) {    
+        $this->wallet_balance=$wallet_balance;
         parent::__construct($uid, $fname, $lname, $email, $phone);
+        $this->customer = new Customer();
     }
-    
-    public function book_room(User $user){
+     // update Customer accordingly to new_data array
+        // new_data array should be an associate array
+        // with key names being the name of the columns
+        // password can be updated here.
+        // e.g [
+        //     "first_name" => '',
+        //     "last_name" => '',
+        //     "phone" => '', max 15 char
+        //     "email" => '',
+        //     "wallet_balance" => 100.90,
+        //     "is_blocked" => 0 or 1,
+        // ];
+    public function add_funds(float $wallet_balance){
+        
+        $this->customer->updateElement($this->email,['wallet_balance' => $wallet_balance]);
+        if (http_response_code() == 201) {
+        echo "Balance updated!";
+        }
+        return;
+    }
+   
+
+    }
+    public function book_room(User $user, Room $room, string $roomnumber){
+    // Fetch available rooms from database, then show available ones. Then update the database to 
+        
+   
+
+    }
+    public function cancel_room(User $user, Room $room, string $roomnumber){
+    // Book the Room
+        
+   
 
     }
 
-    public function requested_service(User $user, string $service){
+    public function service_used(User $user, string $service){
         // Store information for the service requested via Option Tag in the Front End
         // Breakfast
         // Lunch
         // Dinner
         // Parking
         }
-    }
+    
 
     
 
