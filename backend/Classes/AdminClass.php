ea872fd15cd0c30f26fef3b320bc318958c69fd8
Mina Lee <purplemushroomsoup@gmail.com>
2024-08-27 10:42:35 -0700

    feal(AdminClass): Add room add, update, del functions

--- backend/Classes/AdminClass.php
+++ backend/Classes/AdminClass.php
@@ -45,16 +45,74 @@ class AdminController extends User {
 
     
     // Method to manage rooms (add/remove/update)
-    public function add_room(int $roomId, array $roomDetails = []) {
+    public function add_room($room_number, $room_type, $is_available, $room_services, $price_per_night) {
+        try {            
+            $roomDb = new Room();
+            $result = $roomDb->findElement($room_number);
 
+            $decodedResult = json_decode($result, true);
+            if (!empty($decodedResult)) {
+                echo "This Room Number Already Exists";
+                return;
+            }
+
+            $roomDb->createElement($room_number, $room_type, $is_available, $room_services, $price_per_night);
+            if (http_response_code() == 200) {
+                echo "New Room Added";
+            } else {
+                throw new Exception("Room Addition failed", http_response_code());
+            }
+
+        } catch (Exception $error) {
+            $this->staff->LogMessage($this->log_file, $error->getMessage());
+        }
     }
 
-    public function del_room(int $roomId) {
-    
+    public function del_room(int $room_number) {
+        try {
+            $roomDb = new Room();
+            $result = $roomDb->findElement($room_number);
+
+            $decodedResult = json_decode($result, true);
+
+            if (empty($decodedResult)) {
+                echo "This Room Number does not exist";
+                return;
+            }
+
+            $roomDb->deleteElement($room_number);
+            if (http_response_code() == 200) {
+                echo "Room Deleted";
+            } else {
+                throw new Exception("Room deletion failed", http_response_code());
+            }
+
+        } catch (Exception $error) {
+            $this->staff->LogMessage($this->log_file, $error->getMessage());
+        }
     }
 
-    public function update_room(int $roomId, array $roomDetails) {
-    
+    public function update_room(int $room_number, array $new_data) {
+        try {
+            $roomDb = new Room();
+            $result = $roomDb->findElement($room_number);
+
+            $decodedResult = json_decode($result, true);
+
+            if (empty($decodedResult)) {
+                echo "This Room Number does not exist";
+                return;
+            }
+
+            $roomDb->updateElement($room_number, $new_data);
+            if (http_response_code() == 200) {
+                echo "Room Info Updated";
+            } else {
+                throw new Exception("Room update failed", http_response_code());
+            }
+        } catch (Exception $error) {
+            $this->staff->logMessage($this->log_file, $error->getMessage());
+        }
     }
 
     public function closeConnection() {
